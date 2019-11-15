7652b57a953005f7c7f1cd4d16ca8e2afd6b18e6
Kirill-212 <kirya.bolvako@mail.ru>
2019-11-15 18:33:31 +0300

    3-commit for lab10 15.11.2019

---
+++ BKS-2019/BKS-2019/main.cpp
@@ -0,0 +1,49 @@
+#include <cwchar>
+#include <iostream>
+#include "Error.h"
+#include "Parm.h"
+#include "Log.h"
+#include "In.h"
+#include <stack>
+
+
+
+using namespace std;
+
+
+int wmain(int argc, wchar_t* argv[]) {
+
+	setlocale(LC_ALL, "rus");
+	Log::LOG log = Log::INTLOG;
+	try
+	{
+
+		//Parm::PARM parm = Parm::getparm(argc, argv);
+		log = Log::getlog((wchar_t *)L"C:\\ycheba\\main_kyrsovoi\\Course-on-programming-languages\\test_project\\log.log");
+		//wcout << "-in:" << parm.in << ", -out" << parm.out << ", -log: " << parm.log << endl;
+		In::IN in = In::getin((wchar_t *)L"C:\\ycheba\\main_kyrsovoi\\Course-on-programming-languages\\test_project\\test1.txt");
+
+
+		
+
+
+
+		//Log::WriteLine(log, "Тест:", "без ошибок", "");
+		//Log::WriteLog(log);
+		//Log::WriteParm(log, parm);
+		
+		Log::WriteIn(log, in);
+		Log::Close(log);
+
+	}
+	catch (Error::ERROR error) {
+
+		cout << "Ошибка " << error.id << ": " << error.message << ", строка " << error.inext.line << ",позиция " << error.inext.col << std::endl;
+		Log::WriteError(log, error);
+		system("pause");
+	}
+	system("pause");
+}
+
+
+
